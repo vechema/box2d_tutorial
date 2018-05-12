5f9be7cf13839b596e59ca6d6ef4a30335c8d4dc
Jo Egner <vechema@gmail.com>
2018-05-12 17:54:02 -0500

    Tutorial 4, add body factory

--- core/src/com/box2d/tutorial/B2dModel.java
+++ core/src/com/box2d/tutorial/B2dModel.java
@@ -3,6 +3,7 @@ package com.box2d.tutorial;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
@@ -19,6 +20,30 @@ public class B2dModel {
         createFloor();
         createObject();
         createMovingObject();
+
+        // get our body factory singleton
+        BodyFactory bodyFactory = BodyFactory.getInstance(world);
+
+        // Add rubber ball at 1, 1 (middle)
+        bodyFactory.makeCirclePolyBody(1,1,1,BodyFactory.RUBBER, BodyType.DynamicBody, false);
+
+        // Add steel ball at 4, 1 (right)
+        bodyFactory.makeCirclePolyBody(4,1,1,BodyFactory.STEEL, BodyType.DynamicBody,false);
+
+        // add stone ball at -4, 1 (left)
+        bodyFactory.makeCirclePolyBody(-4,1,1,BodyFactory.STONE, BodyType.DynamicBody);
+
+        // add wood wall
+        bodyFactory.makeBoxPolyBody(5,5,2,4,BodyFactory.WOOD, BodyType.StaticBody, false);
+
+        // Add polygon/pentagon platform
+        Vector2[] vertices = new Vector2[5];
+        vertices[0] = new Vector2(1,0);
+        vertices[1] = new Vector2(0,3);
+        vertices[2] = new Vector2(2.5f,5);
+        vertices[3] = new Vector2(5,3);
+        vertices[4] = new Vector2(4,0);
+        bodyFactory.makePolygonShapeBody(vertices, -3, 4,BodyFactory.STONE,BodyType.KinematicBody);
     }
     public void logicStep(float delta) {
         world.step(delta, 3, 3);
@@ -27,7 +52,7 @@ public class B2dModel {
     private void createObject() {
         // create a new body definition (type and location)
         BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.type = BodyType.DynamicBody;
         bodyDef.position.set(0,0);
 
         // add it to the world
@@ -51,7 +76,7 @@ public class B2dModel {
     private void createFloor() {
         // create new body definition (type and location)
         BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.type = BodyType.StaticBody;
         bodyDef.position.set(0,-10);
 
         // add it to the world
@@ -70,7 +95,7 @@ public class B2dModel {
     private void createMovingObject() {
         // create new body definition (type and location)
         BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.KinematicBody;
+        bodyDef.type = BodyType.KinematicBody;
         bodyDef.position.set(0,-12);
 
         // add to the world
---
+++ core/src/com/box2d/tutorial/BodyFactory.java
@@ -0,0 +1,150 @@
+package com.box2d.tutorial;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Meant to be a singleton. Should really be an enum then
+ */
+public class BodyFactory {
+
+    private World world;
+    private static BodyFactory thisInstance;
+
+    public static final int STEEL = 0;
+    public static final int WOOD = 1;
+    public static final int RUBBER = 2;
+    public static final int STONE = 3;
+
+    private static final float DEGTORAD = 0.0174533f;
+
+    private BodyFactory(World world) {
+        this.world = world;
+    }
+
+    public static BodyFactory getInstance(World world) {
+        if (thisInstance == null) {
+            thisInstance = new BodyFactory(world);
+        }
+        return thisInstance;
+    }
+
+    public static FixtureDef makeFixture(int material, Shape shape) {
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+
+        switch (material) {
+            case STEEL:
+                fixtureDef.density = 1f;
+                fixtureDef.friction = 0.3f;
+                fixtureDef.restitution = 0.1f;
+                break;
+            case WOOD:
+                fixtureDef.density = 0.5f;
+                fixtureDef.friction = 0.7f;
+                fixtureDef.restitution = 0.3f;
+                break;
+            case RUBBER:
+                fixtureDef.density = 1f;
+                fixtureDef.friction = 0f;
+                fixtureDef.restitution = 1f;
+                break;
+            case STONE:
+                fixtureDef.density = 1f;
+                fixtureDef.friction = 0.9f;
+                fixtureDef.restitution = 0.01f;
+            default:
+                fixtureDef.density = 7f;
+                fixtureDef.friction = 0.5f;
+                fixtureDef.restitution = 0.3f;
+        }
+        return fixtureDef;
+    }
+
+    public Body makeCirclePolyBody(float posx, float posy, float radius, int material, BodyType bodyType) {
+        return makeCirclePolyBody(posx, posy, radius, material, bodyType,false);
+    }
+
+    public Body makeCirclePolyBody(float posx, float posy, float radius, int material,
+        BodyType bodyType, boolean fixedRotation) {
+        // create the definition
+        BodyDef boxBodyDef = new BodyDef();
+        boxBodyDef.type = bodyType;
+        boxBodyDef.position.x = posx;
+        boxBodyDef.position.y = posy;
+        boxBodyDef.fixedRotation = fixedRotation;
+
+        // create the body to attach said definition
+        Body boxBody = world.createBody(boxBodyDef);
+        CircleShape circleShape = new CircleShape();
+        circleShape.setRadius(radius);
+        boxBody.createFixture(makeFixture(material,circleShape));
+        circleShape.dispose();
+        return boxBody;
+    }
+
+    public Body makeBoxPolyBody(float posx, float posy, float width, float height, int material,
+        BodyType bodyType, boolean fixedRotation) {
+        // create the definition
+        BodyDef boxBodyDef = new BodyDef();
+        boxBodyDef.type = bodyType;
+        boxBodyDef.position.x = posx;
+        boxBodyDef.position.y = posy;
+        boxBodyDef.fixedRotation = fixedRotation;
+
+        // create the body to attach said definition
+        Body boxBody = world.createBody(boxBodyDef);
+        PolygonShape poly = new PolygonShape();
+        poly.setAsBox(width/2, height/2);
+        boxBody.createFixture(makeFixture(material,poly));
+        poly.dispose();
+
+        return boxBody;
+    }
+
+    public Body makePolygonShapeBody(Vector2[] vertices, float posx, float posy, int material, BodyType bodyType) {
+        // create the definition
+        BodyDef boxBodyDef = new BodyDef();
+        boxBodyDef.type = bodyType;
+        boxBodyDef.position.x = posx;
+        boxBodyDef.position.y = posy;
+
+        // create the body to attach said definition
+        Body boxBody = world.createBody(boxBodyDef);
+        PolygonShape polygon = new PolygonShape();
+        polygon.set(vertices);
+        boxBody.createFixture(makeFixture(material,polygon));
+        polygon.dispose();
+
+        return boxBody;
+    }
+
+    public void makeConeSensor(Body body, float size) {
+        FixtureDef fixtureDef = new FixtureDef();
+        //fixtureDef.isSensor = true; // will add in future
+
+        PolygonShape polygon = new PolygonShape();
+
+        float radius = size;
+
+        Vector2[] vertices = new Vector2[5];
+        vertices[0] = new Vector2(0,0);
+        for (int i = 2; i < 6; i++) {
+            float angle = (float) (i / 6.0 * 145 * DEGTORAD);
+            vertices[i - 1] = new Vector2(radius * ((float) MathUtils.cos(angle)),
+                    radius * ((float) MathUtils.sin(angle)));
+        }
+        polygon.set(vertices);
+        fixtureDef.shape = polygon;
+        body.createFixture(fixtureDef);
+        polygon.dispose();
+    }
+}
